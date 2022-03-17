7787b03232fa83bc9deb85eb7e1cc9529c44f0ed
Sang Thanh Nguyen <sangtnguyen@C02F340MML7L.local>
2022-03-17 15:49:44 +0700

    Clean up

--- trainer/__init__.py
+++ trainer/__init__.py
@@ -8,7 +8,7 @@ class GPTTrainer():
         self.path_to_json = path_to_json
 
     def train(self, learning_rate: float = 5e-5, epochs = 1):
-        settings = GENTrainArgs(learning_rate=learning_rate, epochs=epochs)
+        settings = GENTrainArgs(learning_rate=learning_rate, num_train_epochs=epochs)
         dataset = SquadDataset(self.path_to_json)
         path_to_txt = dataset.save_txt('./dataset/train.txt')
         self.model.train(path_to_txt, settings)
